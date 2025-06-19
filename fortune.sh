2a9f6408d95bb1166b0770b1dc13d0a04b145598
Hershy Schwartz <hersheyschwartz1@gmail.com>
2025-06-19 01:52:52 -0400

    modified:   fortune.sh

--- fortune.sh
+++ fortune.sh
@@ -5,27 +5,17 @@
 echo -e "\n~~ Fortune Teller ~~\n"
 
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
-<<<<<<< HEAD
-N=$(( RANDOM % 6 ))
-
-function GET_FORTUNE() {
-=======
 
 N=$(( $RANDOM % 6 ))
 
 
 GET_FORTUNE () {
->>>>>>> 32082cda2116040a2cfa2ebac6a88e8c9ed361db
   if [[ ! $1 ]]
   then
     echo Ask a yes or no question:
   else
     echo Try again. Make sure it ends with a question mark:
   fi
-<<<<<<< HEAD
-
-=======
->>>>>>> 32082cda2116040a2cfa2ebac6a88e8c9ed361db
   read QUESTION
 }
 
@@ -36,8 +26,4 @@ do
   GET_FORTUNE again
 done
 
-<<<<<<< HEAD
 echo -e "\n$RESPONSES[$N]"
-=======
-echo -e "\n${RESPONSES[$N]}"
->>>>>>> 32082cda2116040a2cfa2ebac6a88e8c9ed361db
